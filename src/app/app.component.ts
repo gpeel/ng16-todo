bfd05a68e0fd860e6ca60455fabf1d5dbe248522
Gauthier PEEL <gauthier.peel@gmail.com>
2023-07-04 19:35:33 +0200

    partial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Todo, TodoUtils} from './todo.model';
+
+let count = 0;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng16-todo';
+  // remainingTodos: number = 0;
+  newTodoLabel: string = '';
+
+  tasks: Todo[] = [
+    {id: 0, label: 'Go drink beers', completed: false}, // duck typing works fine!, but beware of id
+    TodoUtils.createTodo('Sleep', true),
+    TodoUtils.createTodo('Play sports')
+    // new Todo(' Sortir ')
+  ];
+
+  removeTodo(task: Todo): void {
+    console.log('REMOVE in APP', task);
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+  }
+
+  addTodo(event: Event): void {
+    console.log('EVENT', event);
+    const input = event.target as HTMLInputElement;
+    console.log('addTodo IN with target.value', input.value);
+    if (input.value.trim()) {
+      console.log('addTodo ok because label not empty');
+      const task = TodoUtils.createTodo(input.value.trim());
+      this.tasks.push(task);
+      input.value = '';
+    }
+    this.newTodoLabel = '';
+  }
+
+  toggleAll(valueChecked: boolean): void {
+    this.tasks.forEach(task => task.completed = valueChecked);
+  }
+
+  computeRemainingTodos(): number {
+    console.log('COMPUTE remaining', count++);
+
+    return this.tasks.filter(task => !task.completed).length;
+  }
+
 }
