3654985fb1221bee80171a55f9e17c5999639f6d
Gauthier PEEL <gauthier.peel@gmail.com>
2023-07-06 11:38:56 +0200

    test step 0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AppComponent} from './app.component';
@@ -8,47 +7,50 @@ import {RemainingMessagePipe} from './remaining-message.pipe';
 import {TodosFilterPipePipe} from './todos-filter.pipe';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule(
-    {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled!: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         TodosFilterPipePipe,
         RemainingMessagePipe,
       ],
       imports: [
-        BrowserModule,
         FormsModule,
         ReactiveFormsModule,
         NgbModule,
         FontAwesomeModule
       ],
-    }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  // Q: could you write the test code for the following test case ?
-  // a:
-  //q: it fails, could you correct it ?
-  //a: not done!
-  // pb was selector querySelectorAll('[data-test="todo-item-li"]');
-  // initialtly it was querySelectorAll('todo-list li'); but it was not working
-  // where dis codepilog get that selector from ?
-  it('should show in the UI the same todos labels and completed values as those in component.todos', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const compiled = fixture.nativeElement;
+    }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    compiled = fixture.nativeElement;
     fixture.detectChanges();
-    const todos = compiled.querySelectorAll('[data-test="todo-item-li"]');
-    expect(todos.length).toBe(app.todos.length);
-    app.todos.forEach((todo, index) => {
-      const todoElement = todos[index];
-      expect(todoElement.querySelector('label').textContent).toBe(todo.label);
-      expect(todoElement.querySelector('input[type=checkbox]').checked).toBe(todo.completed);
-    });
   });
+
+  it('0- should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('0- should render title in a h1 tag', waitForAsync(() => {
+    expect(compiled.querySelector('header h1')!.textContent!.trim()).toContain('Todo App');
+  }));
+
+  // it('should show in the UI the same todos labels and completed values as those in component.todos', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   const compiled = fixture.nativeElement;
+  //   fixture.detectChanges();
+  //   const todos = compiled.querySelectorAll('[data-test="todo-item-li"]');
+  //   expect(todos.length).toBe(app.todos.length);
+  //   app.todos.forEach((todo, index) => {
+  //     const todoElement = todos[index];
+  //     expect(todoElement.querySelector('label').textContent).toBe(todo.label);
+  //     expect(todoElement.querySelector('input[type=checkbox]').checked).toBe(todo.completed);
+  //   });
+  // });
 
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,7 @@
-import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {faChevronDown, faCoffee, faEdit, faRemove} from '@fortawesome/free-solid-svg-icons';
-import {Todo, TODO_FILTER_ENUM, TodoUtils} from './todo.model';
+import {Component} from '@angular/core';
+import {faCoffee, faEdit, faRemove} from '@fortawesome/free-solid-svg-icons';
+import {Todo, TodoUtils} from './todo.model';
 
-let count = 0;
-
-/**
- * Note-4 : also to simplify editing exit we should used a modal instead of a simple input with *ngIf.
- * That way the output scenario is much more controlled (because the user cannot click outside the modal).
- */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,16 +10,8 @@ let count = 0;
 export class AppComponent {
   faCoffee = faCoffee;
   faEdit = faEdit;
-  faChevronDown = faChevronDown;
   faRemove = faRemove;
-  // newTodoLabel: string = '';
-  inputFormControl = new FormControl();
-  inputToggleAllFormControl = new FormControl();
-  @ViewChild('inputEdit', {static: false}) inputEditRef!: ElementRef<HTMLInputElement>;
-  editingTodo: Todo | null = null; // only one Todo could be edited at a time
-  editingTodoLabelPrevious: string | null = null;
-  todosFilterChoice: TODO_FILTER_ENUM = TODO_FILTER_ENUM.ALL;
-  TODO_FILTER_ENUM = TODO_FILTER_ENUM;
+
   todos: Todo[] = [
     {id: 0, label: 'Go drink beers', completed: false}, // duck typing works fine!, but beware of id
     TodoUtils.createTodo('Sleep', true),
@@ -34,119 +19,12 @@ export class AppComponent {
     TodoUtils.createTodo('Go to Mars'),
   ];
 
-  onRemoveTodo(todo: Todo): void {
-    console.log('REMOVE in APP', todo);
-    this.todos.splice(this.todos.indexOf(todo), 1);
-    // Now also check if all todos are completed to toggle the toggleAll checbkox
-    this.todos.every(t => t.completed) ? this.inputToggleAllFormControl.setValue(true) : this.inputToggleAllFormControl.setValue(false);
-  }
+  // onRemoveTodo(todo: Todo): void {}
 
-  // addTodo(value: string, input: HTMLInputElement): void {
-  //   // console.log('EVENT', event);
-  //   // const input = event.target as HTMLInputElement;
-  //   // console.log('addTodo IN with target.value', input.value);
-  //   if (value.trim()) {
-  //     console.log('addTodo ok because label not empty');
-  //     const todo = TodoUtils.createTodo(value.trim());
-  //     this.todos.push(todo);
-  //     input.value = '';
-  //   }
-  //   this.newTodoLabel = '';
-  // }
+  // onAddTodo(): void { }
 
-  onAddTodo(): void {
-    console.log('in ADD TODO FC');
-    if (this.inputFormControl.value?.trim()) {
-      const todo = TodoUtils.createTodo(this.inputFormControl.value.trim());
-      this.todos.push(todo);
-      this.inputFormControl.setValue('');
-    }
-  }
+  // onToggleAll(valueChecked: boolean): void {}
 
-  onEditTodo(todo: Todo): void {
-    console.log('EDIT in APP', todo);
-    if (this.editingTodo === null) {
-      this.editingTodo = todo;
-      this.editingTodoLabelPrevious = todo.label;
-    } else {
-      console.log('already editing another todo => swithcing to this one', todo);
-      this.editingTodo = todo;
-      this.editingTodoLabelPrevious = todo.label;
-    }
-    setTimeout(() => {
-      console.log('REF', this.inputEditRef.nativeElement);
-      this.inputEditRef.nativeElement.focus();
-    });
-  }
+  // onToggleOne(todo: Todo) : void {}
 
-  onInputEdit(value: string, todo: Todo): void {
-    console.log('onInputEdit', value);
-    todo.label = value;
-  }
-
-  onExitEdit() {
-    console.log('onExitEdit');
-    if (this.editingTodo !== null) {
-      if (this.editingTodo.label.trim() === '') {
-        // because we don't want empty labels
-        this.editingTodo.label = this.editingTodoLabelPrevious as string;
-      }
-      this.editingTodo = null;
-      this.editingTodoLabelPrevious = null;
-    }
-  }
-
-  @HostListener('document:keydown', ['$event'])
-  handleEscapeKeyboardEvent(event: KeyboardEvent) {
-    console.log('handleEscapeKeyboardEvent KEYDOWN', event);
-    if (event.key === 'Escape') {
-      if (this.editingTodo !== null) {
-        this.editingTodo.label = this.editingTodoLabelPrevious as string;
-        this.editingTodo = null;
-        this.editingTodoLabelPrevious = null;
-      }
-    }
-  }
-
-  debugKey(l: string, e: any) {
-    console.log('debugKey', l, e);
-  }
-
-  onToggleAll(valueChecked: boolean): void {
-    this.todos.forEach(todo => todo.completed = valueChecked);
-  }
-
-  /**
-   * after a change of completed flag in the array todos, we can now have uncompleted tasks
-   * => So we must check.
-   * If is the case => uncheck the toggleAll checkbox
-   */
-  onToggleCompletedChange(todo: Todo) {
-    todo.completed = !todo.completed;
-    console.log('TOGGLE completed', todo);
-    if (!todo.completed) {
-      console.log('one todo is not completed');
-      this.inputToggleAllFormControl.setValue(false);
-    } else {
-      // if all todos are completed, then check the toggleAll checkbox
-      this.todos.every(t => t.completed) && this.inputToggleAllFormControl.setValue(true);
-    }
-    // check if this todo turns uncompleted
-    // if (this.todos.some(todo => !todo.completed)) {
-    //   console.log('some todos are not completed');
-    // }
-  }
-
-  // computeRemainingTodos(): number {
-  //   console.log('COMPUTE remaining', count++);
-  //   return this.todos.filter(todo => !todo.completed).length;
-  // }
-
-  setTasksFilterStatus(filter: TODO_FILTER_ENUM) {
-    this.todosFilterChoice = filter;
-  }
-
-  isFilterStatus(filter: TODO_FILTER_ENUM) {
-    return this.todosFilterChoice === filter;
-  }
 }
